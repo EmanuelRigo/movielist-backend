966612f44e079e63ead21a53d8cb4d1c7667416b
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-04-01 12:39:52 -0300

    commit

--- src/routers/api/index.router.js
+++ src/routers/api/index.router.js
@@ -5,21 +5,21 @@ import cookiesRouter from "./cookie.router.js";
 import userMoviesRouter from "./userMovies.router.js";
 
 class IndexRouter extends CustomRouter {
+  constructor() {
+    super();
+    console.log("IndexRouter: Inicializando rutas principales...");
+    this.init();
+  }
 
-    constructor(){
-        super()
-        this.init()
-    }
-    init = ()=> {
-        this.use("/movies", ["PUBLIC"], moviesRouter)
-        this.use("/sessions", ["PUBLIC"], sessionRouter)
-        this.use("/cookies", ["PUBLIC"], cookiesRouter)
-        this.use("/usermovies", ["PUBLIC"], userMoviesRouter)
-    }
-
+  init = () => {
+    console.log("IndexRouter: Configurando rutas...");
+    this.use("/movies", ["PUBLIC"], moviesRouter);
+    this.use("/sessions", ["PUBLIC"], sessionRouter);
+    this.use("/cookies", ["PUBLIC"], cookiesRouter);
+    this.use("/usermovies", ["PUBLIC"], userMoviesRouter);
+    console.log("IndexRouter: Rutas configuradas correctamente.");
+  };
 }
 
-let indexRouter = new IndexRouter()
-export default indexRouter.getRouter()
-
-
+let indexRouter = new IndexRouter();
+export default indexRouter.getRouter();
